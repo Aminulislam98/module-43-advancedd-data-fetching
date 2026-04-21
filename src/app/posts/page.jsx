0a8dc7467a974682ae27f355586062d0e43cf2b9
Aminul islam <aminurrohman98@gmail.com>
2026-04-21 14:29:17 +0100

    using try catch api , displayed data

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+export const getPosts = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return await response.json();
+  } catch (err) {
+    throw new Error("Failed to fetch Posts Data.");
+  }
+};
 
-const PostPage = () => {
+const PostPage = async () => {
+  const postsData = await getPosts();
   return (
     <div>
-      <h1 className="text-center text-3xl font-semibold py-4">
+      <h1 className="text-center text-3xl font-semibold py-4 ">
         This is Posts page!
       </h1>
+      <div className="max-w-7xl w-full mx-auto">{postsData.length}</div>
     </div>
   );
 };
